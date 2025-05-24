3a8fccb08692dd0afd06a99a86bea9f27e4a652a
alvinmrrry <alvin_mrrry@outlook.com>
2025-05-24 18:55:02 +0800

    src/static/js/main.js

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -258,6 +258,7 @@ async function connectToWebsocket() {
         generationConfig: {
             responseModalities: responseTypeSelect.value,
             speechConfig: {
+                languageCode: "cmn-CN", // Chinese
                 voiceConfig: { 
                     prebuiltVoiceConfig: { 
                         voiceName: voiceSelect.value    // You can change voice in the config.js file
